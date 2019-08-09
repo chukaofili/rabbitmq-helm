bf9da646bacc04fff783dc871300857e0f750a2b
Chuka Ofili <ofili@microsmart.tk>
2019-08-08 20:52:49 -0500

    Added files

---
+++ helm/install.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+install_helm() {
+  echo "Installing helm ..."
+  kubectl create -f ./helm-install/rbac-config.yaml
+  helm init --service-account tiller --history-max 200
+}
+
+install_helm_client() {
+  echo "Installing helm client..."
+  curl -L https://git.io/get_helm.sh -o ./helm-install/get_helm.sh 
+  chmod 700 ./helm-install/get_helm.sh
+  sh ./helm-install/get_helm.sh
+}
+
+install_nginx_ingress() {
+  echo "Installing nginx ingress ..."
+  kubectl apply -f ./nginx-ingress-install/namespace.yaml
+  helm install --namespace nginx-ingress --name nginx-ingress \
+    -f ./nginx-ingress-install/values_prod.yaml \
+      stable/nginx-ingress
+}
+
+install_cert_manager() {
+  echo "Making sure helm is up to date ..."
+  helm init --upgrade
+  sleep 5
+
+  echo "Installing cert-manager ..."
+  kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.7/deploy/manifests/00-crds.yaml
+  kubectl apply -f ./cert-manager-install/namespace.yaml
+  helm repo add jetstack https://charts.jetstack.io
+  helm repo update
+  helm install \
+    --name cert-manager \
+    --namespace cert-manager \
+    --version v0.7.2 \
+    jetstack/cert-manager
+}
+
+install_http_provider(){
+  source ./cert-manager-install/.env
+
+  echo "Setting up letsencrpt http01 cluster issuers ..."
+  sed "s#__ACCOUNT_EMAIL__#${ACCOUNT_EMAIL}#" ./cert-manager-install/cluster-issuer-http01.template > ./cert-manager-install/cluster-issuer-http01.yaml
+  kubectl apply -f ./cert-manager-install/cluster-issuer-http01.yaml -n cert-manager
+}
+
+install_rabbitmq() {
+  source ./rabbitmq-config/.env
+
+  echo "Making sure helm is up to date ..."
+  helm init --upgrade
+  sleep 5
+
+  echo "Installing rabbitmq ..."
+  helm install --namespace rabbitmq --name rabbitmq \
+    -f ./rabbitmq-config/values_prod.yaml \
+    --set rabbitmq.username="$RABBITMQ_USERNAME" \
+    --set rabbitmq.password="$RABBITMQ_PASSWORD" \
+    --set rabbitmq.erlangCookie="$RABBITMQ_ERLANG_COOKIE" \
+    --set ingress.hostName="$RABBITMQ_PUBLIC_DOMAIN" \
+    --set ingress.tlsSecret="$RABBITMQ_TLS_SECRETNAME" \
+      stable/rabbitmq
+}
+
+echo "Do you wish to install helm? enter 1 or 2:"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes )
+            install_helm
+            break;;
+        No ) break;;
+    esac
+done
+
+echo "Do you wish to install helm client? enter 1 or 2:"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes )
+            install_helm_client
+            break;;
+        No ) break;;
+    esac
+done
+
+echo "Do you wish to install nginx-ingress? enter 1 or 2:"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes )
+            install_nginx_ingress
+            break;;
+        No ) break;;
+    esac
+done
+
+echo "Do you wish to install cert-manager? enter 1 or 2:"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes )
+            install_cert_manager
+            install_http_provider
+            break;;
+        No ) break;;
+    esac
+done
+
+# install_rabbitmq
---
+++ helm/upgrade.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+RABBITMQ_PASSWORD="$(kubectl get secret rabbitmq-rabbitmq --namespace rabbitmq -o jsonpath='{.data.rabbitmq-password}' | base64 --decode)"
+RABBITMQ_ERLANG_COOKIE="$(kubectl get secret rabbitmq-rabbitmq --namespace rabbitmq -o jsonpath='{.data.rabbitmq-erlang-cookie}' | base64 --decode)"
+
+helm upgrade rabbitmq stable/rabbitmq \
+  --set replicas=3 \
+  --set rabbitmq.password="$RABBITMQ_PASSWORD" \
+  --set rabbitmq.erlangCookie="$RABBITMQ_ERLANG_COOKIE"
