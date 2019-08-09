6acf79ae4a54bf0bca91593aa6f6404c03ccb5e7
Chuka Ofili <ofili@microsmart.tk>
2019-08-09 17:28:27 -0500

    Updated secrets

--- install-with-helm.sh
+++ install-with-helm.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+install_helm() {
+  echo "Installing helm ..."
+  kubectl apply -f ./helm-install/rbac-config.yaml
+  helm init --service-account tiller --history-max 200
+}
+
+install_helm_client() {
+  echo "Installing helm client..."
+  curl -L https://git.io/get_helm.sh -o ./helm-install/get_helm.sh 
+  chmod 700 ./helm-install/get_helm.sh
+  ./helm-install/get_helm.sh
+}
+
 install_nginx_ingress() {
   echo "Installing nginx ingress ..."
   kubectl apply -f ./nginx-ingress-install/namespace.yaml
@@ -36,7 +49,7 @@ install_http_provider(){
   rm ./cert-manager-install/cluster-issuer-http01.tmp.yaml
 }
 
-install_rabbitmq_k8s() {
+install_rabbitmq_using_helm() {
   source ./rabbitmq-config/.env
   RABBITMQ_USERNAME=${RABBITMQ_USERNAME:?"Must provide RABBITMQ_USERNAME in ./rabbitmq-config/.env file. e.g. user"}
   RABBITMQ_PASSWORD=${RABBITMQ_PASSWORD:?"Must provide RABBITMQ_PASSWORD in ./rabbitmq-config/.env file. e.g. password"}
@@ -44,40 +57,61 @@ install_rabbitmq_k8s() {
   RABBITMQ_PUBLIC_DOMAIN=${RABBITMQ_PUBLIC_DOMAIN:?"Must provide RABBITMQ_PUBLIC_DOMAIN in ./rabbitmq-config/.env file. e.g. rmq.example.com"}
   RABBITMQ_TLS_SECRETNAME=${RABBITMQ_TLS_SECRETNAME:?"Must provide RABBITMQ_TLS_SECRETNAME in ./rabbitmq-config/.env file. e.g. rmq-example-tls"}
 
-  RABBITMQ_USERNAME_BASE64=$(echo -ne "$RABBITMQ_USERNAME" | base64);
-  RABBITMQ_PASSWORD_BASE64=$(echo -ne "$RABBITMQ_PASSWORD" | base64);
-  RABBITMQ_ERLANG_COOKIE_BASE64=$(echo -ne "$RABBITMQ_ERLANG_COOKIE" | base64);
+  echo "Making sure helm is up to date ..."
+  helm init --upgrade
+  sleep 5
 
-  echo "Installing rabbitmq using deployment files..."
-  sed "s#__RABBITMQ_USERNAME__#${RABBITMQ_USERNAME}#" ./deployment/secret.template > ./deployment/secret.tmp.yaml
-  sed -i .bak "s#__RABBITMQ_USERNAME_BASE64__#${RABBITMQ_USERNAME_BASE64}#" ./deployment/secret.tmp.yaml
-  sed -i .bak "s#__RABBITMQ_PASSWORD__#${RABBITMQ_PASSWORD}#" ./deployment/secret.tmp.yaml
-  sed -i .bak "s#__RABBITMQ_PASSWORD_BASE64__#${RABBITMQ_PASSWORD_BASE64}#" ./deployment/secret.tmp.yaml
-  sed -i .bak "s#__RABBITMQ_ERLANG_COOKIE_BASE64__#${RABBITMQ_ERLANG_COOKIE_BASE64}#" ./deployment/secret.tmp.yaml
-  sed "s#__RABBITMQ_PUBLIC_DOMAIN__#${RABBITMQ_PUBLIC_DOMAIN}#" ./deployment/ingress.template > ./deployment/ingress.tmp.yaml
-  sed -i .bak "s#__RABBITMQ_TLS_SECRETNAME__#${RABBITMQ_TLS_SECRETNAME}#" ./deployment/ingress.tmp.yaml
-  rm ./deployment/*.bak
-#   kubectl apply -f ./deployment
+  echo "Installing rabbitmq ..."
+  kubectl apply -f ./rabbitmq-config/namespace.yaml
+  sed "s#__RABBITMQ_PASSWORD__#${RABBITMQ_PASSWORD}#" ./rabbitmq-config/definitions.template > ./rabbitmq-config/definitions.tmp.yaml
+  kubectl apply -f ./rabbitmq-config/definitions.tmp.yaml
+  rm ./rabbitmq-config/definitions.tmp.yaml
+  helm install --namespace rabbitmq --name rabbitmq \
+    -f ./rabbitmq-config/values_prod.yaml \
+    --set rabbitmq.username="$RABBITMQ_USERNAME" \
+    --set rabbitmq.password="$RABBITMQ_PASSWORD" \
+    --set rabbitmq.erlangCookie="$RABBITMQ_ERLANG_COOKIE" \
+    --set ingress.hostName="$RABBITMQ_PUBLIC_DOMAIN" \
+    --set ingress.tlsSecret="$RABBITMQ_TLS_SECRETNAME" \
+      stable/rabbitmq
 }
-
-install_rabbitmq_k8s
-exit 1;
 
 get_loadbalancer_ip(){
     echo "================================================================================================="
     SERVICE_IP=$(kubectl get svc nginx-ingress-controller --namespace nginx-ingress --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
     echo "Modify your dns records to point your domain ${RABBITMQ_PUBLIC_DOMAIN} to ${SERVICE_IP}"
     echo "You can visit https://${RABBITMQ_PUBLIC_DOMAIN} to access the managment plugin"
-    echo "You can use amqp://${RABBITMQ_USERNAME}:${RABBITMQ_PASSWORD}@${RABBITMQ_PUBLIC_DOMAIN} to access the rabbitmq. The default queue is named 'default'"
     echo "================================================================================================="
 }
+
+echo "Do you wish to install helm? enter 1 or 2:"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes )
+            install_helm
+            sleep 5
+            break;;
+        No ) break;;
+    esac
+done
+
+echo "Do you wish to install helm client? enter 1 or 2:"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes )
+            install_helm_client
+            sleep 5
+            break;;
+        No ) break;;
+    esac
+done
 
 echo "Do you wish to install nginx-ingress? enter 1 or 2:"
 select yn in "Yes" "No"; do
     case $yn in
         Yes )
             install_nginx_ingress
-            sleep 15
+            sleep 5
             break;;
         No ) break;;
     esac
@@ -96,11 +130,11 @@ select yn in "Yes" "No"; do
     esac
 done
 
-echo "Do you wish to install rabbitmq? enter 1 or 2:"
+echo "Do you wish to install rabbitmq using helm? enter 1 or 2:"
 select yn in "Yes" "No"; do
     case $yn in
         Yes )
-            install_rabbitmq_k8s
+            install_rabbitmq_using_helm
             get_loadbalancer_ip
             break;;
         No ) break;;
